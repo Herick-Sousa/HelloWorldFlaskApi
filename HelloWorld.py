a619aeaace6afd1ce3bdd7153998f3e27c8dd8ec
Herick-Sousa <113268489+Herick-Sousa@users.noreply.github.com>
2023-10-19 07:54:52 -0300

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,5 +3,8 @@ app =Flask(__name__)
 
 @app.route('/')
 def index():
+    with open("Test.txt", "w") as file:
+        file.write("Teste")
+    
     return 'Web application python Deploying on Zeet '
 app.run(host='0.0.0.0',port=81)
